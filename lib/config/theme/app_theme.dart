5e02513da4bb4a82a2d0044b13d3203d86640493
Elmer Jacobo <ejacobotiniano@gmail.com>
2023-07-28 22:03:53 -0500

    Home screen create

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+  Colors.purple,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink,
+  Colors.teal,
+  Colors.brown,
+  Colors.cyan,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0,
+  })  : assert(selectedColor >= 0, 'selected color must be greater then 0'),
+        assert(selectedColor < colorList.length,
+            'Selected color must be less than ${colorList.length}');
+
+  ThemeData getTheme() => ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: colorList[selectedColor],
+      appBarTheme: const AppBarTheme(centerTitle: false));
+}
