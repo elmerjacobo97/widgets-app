95481a5b6ec9b26ee8cccae99ab73a8301681b8c
Elmer Jacobo <ejacobotiniano@gmail.com>
2023-07-28 22:26:54 -0500

    Home Screen implemented

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,8 +7,47 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Text('Flutter + Material 3'),
-    ));
+      appBar: AppBar(
+        title: const Text('Flutter + Material 3'),
+      ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      // physics: const BouncingScrollPhysics(), // Quitar la animación de rebote
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItems = appMenuItems[index];
+        return _CustomListTitle(menuItems: menuItems);
+      },
+    );
+  }
+}
+
+class _CustomListTitle extends StatelessWidget {
+  const _CustomListTitle({
+    required this.menuItems,
+  });
+
+  final MenuItem menuItems;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItems.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_outlined, color: colors.primary),
+      title: Text(menuItems.title),
+      subtitle: Text(menuItems.subTitle),
+      onTap: () {},
+    );
   }
 }
