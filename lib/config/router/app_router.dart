11bf1ee0b4a8bf1a39e74fda441372f84580f0c1
Elmer Jacobo <ejacobotiniano@gmail.com>
2023-07-29 06:22:33 -0500

    rutas con nombre implementado (go_router)

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -4,14 +4,17 @@ import 'package:widgets_app/presentation/screens/screens.dart';
 final appRouter = GoRouter(initialLocation: '/', routes: [
   GoRoute(
     path: '/',
+    name: HomeScreen.routeName,
     builder: (context, state) => const HomeScreen(),
   ),
   GoRoute(
     path: '/buttons',
+    name: ButtonsScreen.routeName,
     builder: (context, state) => const ButtonsScreen(),
   ),
   GoRoute(
     path: '/cards',
+    name: CardsScreen.routeName,
     builder: (context, state) => const CardsScreen(),
   )
 ]);
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/carts/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String routeName = 'home_screen';
+
   const HomeScreen({super.key});
 
   @override
@@ -58,6 +61,9 @@ class _CustomListTitle extends StatelessWidget {
 
         // Con go_router.
         context.push(menuItems.link);
+        // context.pushNamed(
+        //   CardsScreen.routeName,
+        // );
       },
     );
   }
