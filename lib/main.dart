ecfa2b39f787975475a690d632e96f1f8e602824
Elmer Jacobo <ejacobotiniano@gmail.com>
2023-07-28 22:52:46 -0500

    Rutas basicas configuradas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/carts/cards_screen.dart';
 import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
@@ -15,6 +17,11 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 2).getTheme(),
       home: const HomeScreen(),
+      // Nombre de rutas.
+      routes: {
+        '/buttons': (context) => const ButtonsScreen(),
+        '/cards': (context) => const CardsScreen(),
+      },
     );
   }
 }
